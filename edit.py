159c64438a303e3800ab6492b367985441450a9e
CubaHub <Pandya967@gmail.com>
2016-07-18 00:32:49 -0400

    Adding version with commands

--- edit.py
+++ edit.py
@@ -4,26 +4,31 @@ import sys
 system("clear")
 counter = 0
 print("Welcome to Bill, the world's most minimal editor  ")
+def commandmode():  # How'd it get burned
+   while True:
+       command = raw_input(">")
+       if "killbill" in command:
+           sys.exit()
+       elif "return" in command:
+        break
+       elif "help" in command:
+        print("Bill Commands\r\n1: 'Help' - prints this text\r\n 2: 'Return' - returns to editing mode from command mode\r\n 3: 'killbill' - Exits the editor")
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = raw_input("filename:")
 
-if len(sys.argv)>1: filename = sys.argv[1]
-else:               filename = raw_input("filename:")
-
-file = open(filename,"a+")
-print "opened "+filename+" for editing"
+file = open(filename, "a+")
+print "opened " + filename + " for editing"
 
 for line in file:
-    counter+=1
-    print counter,line.strip()
+    counter += 1
+    print counter, line.strip()
 
 while True:
     try:
-        counter+=1
+        counter += 1
         input = raw_input(counter)
-        if "/killbill" in input:
-            sys.exit()
-        else:
-            file.write(input+"\r\n")
-    except(KeyboardInterrupt):			
-        exit = raw_input("\r\nAre you sure you'd like to exit? y/n")
-        if "y" in exit: sys.exit()
-        elif "n" in exit: pass 
+        file.write(input + "\r\n")
+    except(KeyboardInterrupt):
+        commandmode()
